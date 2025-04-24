5561dcc77e5fbc5ae5ea44730ebc48712770a3bd
Quroboros <ouroborosq54@gmail.com>
2025-04-24 16:57:55 +0300

    1.1 start

--- gamerule.py
+++ gamerule.py
@@ -3,36 +3,36 @@ class Field:
     #field = []
 
     def __init__(self, sizeX, sizeY):
-        self.sizeX = sizeX
-        self.sizeY = sizeY
-        self.CellPos = []
+        self.__sizeX = sizeX
+        self.__sizeY = sizeY
+        self.__CellPos = []
 
 
     def setCells(self, CellPos):
-        self.CellPos = CellPos
+        self.__CellPos = CellPos
         
         '''
-        for i in range(self.sizeY):
+        for i in range(self.__sizeY):
             self.field.append([])
-            for j in range(self.sizeX):
+            for j in range(self.__sizeX):
                 self.field[i].append(0)
         
-        for i in CellPos:
+        for i in __CellPos:
             self.field[i[0]][i[1]] = 1
         '''
         
 
     def clearField(self):
-        self.CellPos = []
+        self.__CellPos = []
 
 
     def returnCells(self):
-        return self.CellPos
+        return self.__CellPos
 
     def step(self):
-        CellPosNext = []
-        for y in range(self.sizeY):
-            for x in range(self.sizeX):
+        __CellPosNext = []
+        for y in range(self.__sizeY):
+            for x in range(self.__sizeX):
                 count = 0
                 
                 duration = [
@@ -43,26 +43,28 @@ class Field:
                 
                 for dx, dy in duration:
                     nx, ny = dx+x, dy+y
-                    if 0 <= nx < self.sizeX and 0 <= ny < self.sizeY:
-                        if [nx, ny] in self.CellPos:
+                    if 0 <= nx < self.__sizeX and 0 <= ny < self.__sizeY:
+                        if [nx, ny] in self.__CellPos:
                             count += 1
                     pass
 
                 if (x == 1) and (y == 1):
                     print(count)
                 
-                if (count > 1) and (count < 4):
-                    CellPosNext.append([x, y])
+                if [x, y] in self.__CellPos and (count >= 2) and (count <= 3):
+                    __CellPosNext.append([x, y])
+                if [x, y] not in self.__CellPos and (count == 3):
+                    __CellPosNext.append([x, y])
                 
-        self.CellPos = CellPosNext
+        self.__CellPos = __CellPosNext
         pass
 
 
     def printfield(self):
-        for y in range(self.sizeY):
-            for x in range(self.sizeX):
-                #print('{0} в {1}'.format([x, y], self.CellPos))
-                if [x, y] in self.CellPos:
+        for y in range(self.__sizeY):
+            for x in range(self.__sizeX):
+                #print('{0} в {1}'.format([x, y], self.__CellPos))
+                if [x, y] in self.__CellPos:
                     print(1, end='')
                 else:
                     print(0, end='')
---
+++ main.py
@@ -0,0 +1,67 @@
+import pygame
+import gamerule
+import os
+
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+# Инициализация окна
+pygame.init()
+width, height = 1000, 1000
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Игра Жизнь")
+
+# Создание таймера
+clock = pygame.time.Clock()
+
+# Создание рабочего поля
+fieldSize = [20, 100]
+field = gamerule.Field(fieldSize[0], fieldSize[1])
+
+field.setCells([[2, 1], 
+                [3, 2],
+                [1, 3], [2, 3], [3, 3]])
+'''
+field.setCells([[2, 1], 
+                [2, 2],
+                [2, 3]])
+'''
+                
+# Инициализация цикла игры
+running = True
+while running:
+
+    # Обработка события выхода
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Отчистка экрана
+    screen.fill((0, 0, 0))  
+
+    # Размета поля
+    for i in range(0, height, height//fieldSize[1]):
+        pygame.draw.line(screen, 'white', (i, 0), (i, height), width=1)
+    for i in range(0, width, width//fieldSize[0]):
+        pygame.draw.line(screen, 'white', (0, i), (width, i), width=1)
+
+    # Вывод поля в терминал
+    clear_terminal()
+    # field.printfield()
+    cells = field.returnCells()
+    print('------------------------------------')
+   
+    # Закрашивание активных ячеек
+    for y in range(fieldSize[1]):
+        for x in range(fieldSize[1]):
+            if [x, y] in cells:
+                pygame.draw.rect(screen, 'white', (x*(height//fieldSize[0]), y*(height//fieldSize[1]), (height//fieldSize[0]), (height//fieldSize[1])))
+
+    # Обработка времени
+    field.step()
+
+    # Обновление экрана
+    pygame.display.flip()  
+    clock.tick(5)  # 60 кадров в секунду
